f04787740f1b430b2ce4b0a0d0ac522fe8b3ad46
GLinBoy <hojjatabedie@gmail.com>
2022-05-29 17:40:18 +0200

    📦 NEW: Added consumer module

---
+++ consumer/src/test/kotlin/com/glinboy/demo/websocket/consumer/ConsumerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glinboy.demo.websocket.consumer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
