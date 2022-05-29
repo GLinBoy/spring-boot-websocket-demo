4d7685857d4282fb5656423f854fbcec3ee0c0e1
GLinBoy <hojjatabedie@gmail.com>
2022-05-29 17:29:07 +0200

    🕯 START: Initiated the project

---
+++ src/main/kotlin/com/glinboy/demo/websocket/SpringBootWebSocketDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.glinboy.demo.websocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootWebSocketDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootWebSocketDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/glinboy/demo/websocket/SpringBootWebSocketDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glinboy.demo.websocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootWebSocketDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
