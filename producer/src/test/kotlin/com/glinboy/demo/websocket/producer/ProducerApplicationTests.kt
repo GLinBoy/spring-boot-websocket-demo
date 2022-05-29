71b7dbefc0cb018277f8a8046d6dedc35d86ac8b
GLinBoy <hojjatabedie@gmail.com>
2022-05-29 17:39:06 +0200

    📦 NEW: Added producer module

---
+++ producer/src/test/kotlin/com/glinboy/demo/websocket/producer/ProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glinboy.demo.websocket.producer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
