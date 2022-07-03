8f7baa249e939793d80ff286a303527107992ca7
GLinBoy <hojjatabedie@gmail.com>
2022-07-03 16:56:40 +0200

    👌 IMPROVE: Add websocket configuration

---
+++ consumer/src/main/kotlin/com/glinboy/demo/websocket/consumer/config/WebSocketConfig.kt
@@ -0,0 +1,12 @@
+package com.glinboy.demo.websocket.consumer.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+}
