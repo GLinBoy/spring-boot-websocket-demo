70b309885a72e3114455a0fb5514ab3b233abe1d
GLinBoy <hojjatabedie@gmail.com>
2022-07-03 17:00:00 +0200

    👌 IMPROVE: Added a controller

---
+++ consumer/src/main/kotlin/com/glinboy/demo/websocket/consumer/web/websocket/GreetingController.kt
@@ -0,0 +1,12 @@
+package com.glinboy.demo.websocket.consumer.web.websocket
+
+import com.glinboy.demo.websocket.consumer.dto.Greeting
+import com.glinboy.demo.websocket.consumer.dto.HelloMessage
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.stereotype.Controller
+import org.springframework.web.util.HtmlUtils
+
+@Controller
+class GreetingController {
+}
