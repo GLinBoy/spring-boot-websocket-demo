8bf5ace08b49e8d810c297d7f1c974336b75d5a6
GLinBoy <hojjatabedie@gmail.com>
2022-07-04 20:24:12 +0200

    👌 IMPROVE: Added webjars dependencies

--- web-app/build.gradle.kts
+++ web-app/build.gradle.kts
@@ -21,6 +21,11 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.webjars:webjars-locator-core")
+    implementation("org.webjars:sockjs-client:1.0.2")
+    implementation("org.webjars:stomp-websocket:2.3.3")
+    implementation("org.webjars:bootstrap:3.3.7")
+    implementation("org.webjars:jquery:3.1.1-1")
 }
 
 tasks.withType<KotlinCompile> {
