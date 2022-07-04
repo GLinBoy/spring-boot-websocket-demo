a563e01c8108ababae0471a380379ac4f96af087
GLinBoy <hojjatabedie@gmail.com>
2022-07-04 20:25:56 +0200

    👌 IMPROVE: Added connected status updater to the app.js file

---
+++ web-app/src/main/resources/static/app.js
@@ -0,0 +1,13 @@
+var stompClient = null;
+
+function setConnected(connected) {
+  $("#connect").prop("disabled", connected);
+  $("#disconnect").prop("disabled", !connected);
+  if (connected) {
+    $("#conversation").show();
+  }
+  else {
+    $("#conversation").hide();
+  }
+  $("#greetings").html("");
+}
