aae9e134670f953586cc804dfe91beb7ec3f47c1
GLinBoy <hojjatabedie@gmail.com>
2022-10-25 21:05:27 +0200

    📦 NEW: Added Gradle plugin to check dependencies version on consumer app
    resolved #80

--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     id("org.ec4j.editorconfig") version "0.0.3"
+    id("com.github.ben-manes.versions") version "0.43.0"
 }
 
 group = "com.glinboy.demo.websocket"
