e93a001e2d04e70e5ca0dc587a206f84fe729e19
GLinBoy <hojjatabedie@gmail.com>
2022-10-26 11:14:37 +0200

    👌 IMPROVE: Upgraded stomp-websocket [2.3.3 -> 2.3.4]

--- web-app/build.gradle.kts
+++ web-app/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.webjars:webjars-locator-core")
     implementation("org.webjars:sockjs-client:1.5.1")
-    implementation("org.webjars:stomp-websocket:2.3.3")
+    implementation("org.webjars:stomp-websocket:2.3.4")
     implementation("org.webjars:bootstrap:3.3.7")
     implementation("org.webjars:jquery:3.1.1-1")
 }
