743dc1e2415a2c2f2974f019cab7106267edecf2
GLinBoy <hojjatabedie@gmail.com>
2023-12-02 19:46:51 +0100

    👌 IMPROVE: Upgraded Java of consumer app to 21
    resolved #217

--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 group = "com.glinboy.demo.websocket"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 val jacksonModuleVersion = "2.15.1"
 
@@ -32,7 +32,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
