465c1a85f24ff7c688cbc8dbe20f67ab2bd5218c
GLinBoy <hojjatabedie@gmail.com>
2023-12-03 19:14:24 +0100

    👌 IMPROVE: Enhanced producer app Gradle imports
    resolved #225

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -18,9 +18,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
