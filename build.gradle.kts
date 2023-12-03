1b4e87483ef2bfafe3abf602204899a9c484edb0
GLinBoy <hojjatabedie@gmail.com>
2023-12-03 19:14:01 +0100

    👌 IMPROVE: Enhanced main app Gradle imports
    resolved #224

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
