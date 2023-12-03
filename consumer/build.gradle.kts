452c990f9807e43c04bd47d183dd62ad2721d979
GLinBoy <hojjatabedie@gmail.com>
2023-12-03 19:14:47 +0100

    👌 IMPROVE: Enhanced consumer app Gradle imports
    resolved #226

--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -20,13 +20,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonModuleVersion")
 }
 
 tasks.withType<KotlinCompile> {
