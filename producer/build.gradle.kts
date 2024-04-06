448dcfdd01c68d771dce5e1448f8cc7d32ee1055
GLinBoy <glinboy@live.com>
2024-04-06 10:59:41 +0200

    👌 IMPROVE: Upgraded Kotlin of the producer to 1.9.23
    resolved #266

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
     id("org.ec4j.editorconfig") version "0.0.3"
     id("com.github.ben-manes.versions") version "0.50.0"
 }
