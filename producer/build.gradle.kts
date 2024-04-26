3b3e6facd2c23fecc8f03a737009ba1bf6c68978
GLinBoy <glinboy@live.com>
2024-04-26 19:09:54 +0200

    👌 IMPROVE: Upgraded dependency of the producer editorconfig.gradle.plugin [0.0.3 -> 0.1.0]
    #274

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
-    id("org.ec4j.editorconfig") version "0.0.3"
+    id("org.ec4j.editorconfig") version "0.1.0"
     id("com.github.ben-manes.versions") version "0.50.0"
 }
 
